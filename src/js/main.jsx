eea09504a5769cafcd973c1e67339ed531362ed2
Lucas Matias Calvo <lucascalvo2903@gmail.com>
2025-10-14 04:06:22 +0000

    borrador

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -11,8 +11,20 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+
+
+
+let root = ReactDOM.createRoot(document.getElementById('root'));
+let contador = 0;
+
+setInterval(() => {
+  let two = Math.floor((contador / 10) % 10);
+  let one = Math.floor((contador / 1) % 10);
+  contador++;
+
+  root.render(
+    <React.StrictMode>
+      <Home digitOne={one} digitTwo={two} />
+    </React.StrictMode>
+  );
+}, 1000);
