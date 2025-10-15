018ac696919341c2fac41fc14f97991b24e408c8
Lucas Matias Calvo <lucascalvo2903@gmail.com>
2025-10-15 02:53:44 +0000

    por la mitad

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import Card from "./Card.jsx";
+
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import { preprocessCSS } from "vite";
 
 //create your first component
+
 const Home = (props) => {
   return (
-    <div>
-      <div>{props.digitTwo}</div>
-      <div>{props.digitOne}</div>
+    <div className="text-center mt-5">
+      <h1>⏱️ Contador React</h1>
+      <div className="d-flex justify-content-center gap-2 fs-1">
+        <div className="bg-dark text-white p-3 rounded">{props.digitSix}</div>
+        <div className="bg-dark text-white p-3 rounded">{props.digitFive}</div>
+        <div className="bg-dark text-white p-3 rounded">{props.digitFour}</div>
+        <div className="bg-dark text-white p-3 rounded">{props.digitThree}</div>
+        <div className="bg-dark text-white p-3 rounded">{props.digitTwo}</div>
+        <div className="bg-dark text-white p-3 rounded">{props.digitOne}</div>
+      </div>
     </div>
   );
 };
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,30 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
 // index.css'
-import '../styles/index.css'
+import '../styles/index.css';
 
 // components
 import Home from './components/Home';
 
 
-
-
-let root = ReactDOM.createRoot(document.getElementById('root'));
+let root = ReactDOM.createRoot(document.getElementById("root"));
 let contador = 0;
 
 setInterval(() => {
+  let six = Math.floor((contador / 100000) % 10);
+  let five = Math.floor((contador / 10000) % 10);
+  let four = Math.floor((contador / 1000) % 10);
+  let three = Math.floor((contador / 100) % 10);
   let two = Math.floor((contador / 10) % 10);
   let one = Math.floor((contador / 1) % 10);
   contador++;
 
   root.render(
     <React.StrictMode>
-      <Home digitOne={one} digitTwo={two} />
+      <Home
+        digitOne={one}
+        digitTwo={two}
+        digitThree={three}
+        digitFour={four}
+        digitFive={five}
+        digitSix={six}
+      />
     </React.StrictMode>
   );
 }, 1000);
