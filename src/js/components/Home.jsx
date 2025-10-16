1fe89e0add1e6641f557d49a4ee06a787f970f2a
Lucas Matias Calvo <lucascalvo2903@gmail.com>
2025-10-16 03:17:20 +0000

    contador final

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,21 @@
 import React from "react";
-import Card from "./Card.jsx";
-
-
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import { preprocessCSS } from "vite";
-
-//create your first component
 
 const Home = (props) => {
   return (
     <div className="text-center mt-5">
-      <h1>⏱️ Contador React</h1>
-      <div className="d-flex justify-content-center gap-2 fs-1">
-        <div className="bg-dark text-white p-3 rounded">{props.digitSix}</div>
-        <div className="bg-dark text-white p-3 rounded">{props.digitFive}</div>
-        <div className="bg-dark text-white p-3 rounded">{props.digitFour}</div>
-        <div className="bg-dark text-white p-3 rounded">{props.digitThree}</div>
-        <div className="bg-dark text-white p-3 rounded">{props.digitTwo}</div>
-        <div className="bg-dark text-white p-3 rounded">{props.digitOne}</div>
+      <h1>Contador React</h1>
+      <div className="d-flex justify-content-center bg-dark text-white fs-1">
+        <div className="p-2">{props.digitSix}</div>
+        <div className="p-2 ">{props.digitFive}</div>
+        <div className="p-2">{props.digitFour}</div>
+        <div className="p-2">{props.digitThree}</div>
+        <div className="p-2">{props.digitTwo}</div>
+        <div className="p-2">{props.digitOne}</div>
       </div>
     </div>
   );
 };
 
 export default Home;
+
+
